cd67f0a6bff6714d06e7111d49db1d70e343d0e8
Jonathan Hernandez <jonathanhernandez@Laptop-de-Jonathan.local>
2025-07-08 18:41:44 -0500

    Coreccion

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home  from '@/views/Home.vue'
-import Navbar   from '@/components/Navbar.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import Home from '@/views/Home.vue'
+import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
 
 import Select from '@/views/Select.vue'
@@ -12,42 +13,42 @@ const routes = [
     path: '/',
     name: 'home',
     components: {
-      default: Home,   
+      default: Home,
       navbar: Navbar,
-      footer: Footer
-    }
+      footer: Footer,
+    },
   },
-    {
+  {
     path: '/select',
     name: 'select',
     components: {
-      default: Select,   
+      default: Select,
       navbar: Navbar,
-      footer: Footer
-    }
+      footer: Footer,
+    },
   },
-      {
+  {
     path: '/confirm',
     name: 'confirm',
     components: {
-      default: Confirm,   
+      default: Confirm,
       navbar: Navbar,
-      footer: Footer
-    }
+      footer: Footer,
+    },
   },
-        {
+  {
     path: '/succes',
     name: 'succes',
     components: {
-      default: Succes,   
+      default: Succes,
       navbar: Navbar,
-      footer: Footer
-    }
+      footer: Footer,
+    },
   },
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(), // 👈 compatible con GitHub Pages
   routes,
 })
 
